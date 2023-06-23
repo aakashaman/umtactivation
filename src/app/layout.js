02fa825cb35e46c42b89bab437949b9cb3d60077
aakashaman <aakashaman9931@gmail.com>
2023-06-23 22:02:48 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+'use client'
+import { ThemeProvider } from '@material-tailwind/react'
+import Navba from './components/Navba'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -10,8 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ThemeProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navba />
+        {children}</body>
     </html>
+    </ThemeProvider>
   )
 }
